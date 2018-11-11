9fc3ed61729ccd9fd4ed8f7ac4f9471cf2fb7bf1
roman-oleynik <raman.alejnik@gmail.com>
2018-11-11 11:20:46 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,481 @@
+const json = {
+	"data": [
+		{
+			"variant": "1",
+			"questions" : [
+				
+				{
+					"question": "Пишется ь на месте пропуска в словах:",
+					"answers": [
+						{
+							"1":"пижамоч..ка",
+							"2":"бур..ян",
+							"3":"увлеч..cя",
+							"4":"много неудач..",
+							"5":"без..ядерный"
+						}
+					],
+					"correct-answers": "2,3",
+					"explaining": "explaining"
+				},
+				{
+					"question": "Двойные согласные есть в словах:",
+					"answers": [
+						{
+							"1":"ра(с/сс)писаться в журнале",
+							"2":"бе(с/сс)мысленный",
+							"3":"га(л/лл)ерея",
+							"4":"кора(л/лл)овый",
+							"5":"эскимо(с/сс)кий"
+						}
+					],
+					"correct-answers": "2,4,5",
+					"explaining": "explaining"
+				},
+				{
+					"question": "Пишется и на месте пропуска в словах:",
+					"answers": [
+						{
+							"1":"пр..клеить к конверту",
+							"2":"пр..стыдить лентяя",
+							"3":"из пр..данья старины глубокой",
+							"4":"пр..образовать что-либо",
+							"5":"пр..вивка против гриппа"
+						}
+					],
+					"correct-answers": "1,2,5",
+					"explaining": "explaining"
+				},
+				{
+					"question": "Одна и та же буква пишется на месте всех пропусков в рядах:",
+					"answers": [
+						{
+							"1":"к..пировать статью, эксп..нат",
+							"2":"зам..реть от страха, гард..роб",
+							"3":"возгл..влять, подг..ревший суп",
+							"4":"подр..стать, возл..жение венков",
+							"5":"неув..даемая красота, загл..деться"
+						}
+					],
+					"correct-answers": "1,2,5",
+					"explaining": "explaining"
+				},
+				{
+					"question": "Пишется ы на месте пропуска в словах:",
+					"answers": [
+						{
+							"1":"автоинспекц..я",
+							"2":"две долгожительниц..",
+							"3":"спец..алист",
+							"4":"без..нвентарный",
+							"5":"сверх..нтересный"
+						}
+					],
+					"correct-answers": "2,4",
+					"explaining": "explaining"
+				},
+				{
+					"question": "Пишется о на месте пропуска в словах:",
+					"answers": [
+						{
+							"1":"беч..вка",
+							"2":"с грач..м",
+							"3":"капюш..н",
+							"4":"галч..нок",
+							"5":"уч..ность"
+						}
+					],
+					"correct-answers": "2,3,4",
+					"explaining": "explaining"
+				},
+				{
+					"question": "Пишется и на месте всех пропусков в рядах:",
+					"answers": [
+						{
+							"1":"за крадущ..мся котом, о печал..",
+							"2":"песоч..к, каракул..вый",
+							"3":"плать..це, при обморожени..",
+							"4":"по нынешн..м временам, уродл..вый",
+							"5":"в зыбуч..м песке, обрывч..тый"
+						}
+					],
+					"correct-answers": "1,3,4",
+					"explaining": "explaining"
+				},
+				{
+					"question": "Пишется е на месте пропуска в словах:",
+					"answers": [
+						{
+							"1":"мы заверт..мся",
+							"2":"он укро..тся",
+							"3":"перешаг..вать",
+							"4":"заменя..мый",
+							"5":"он зате..т спор"
+						}
+					],
+					"correct-answers": "2,4,5",
+					"explaining": "explaining"
+				},
+				{
+					"question": "Пишется нн на месте всех пропусков в рядах:",
+					"answers": [
+						{
+							"1":"давно не белё..ые стены, говорить сдержа..о",
+							"2":"разброса..ый, мощё..ая дорога",
+							"3":"тополи..ый пух, ответы тщательно продума..ы",
+							"4":"прославле..ый, преда..ость",
+							"5":"рафинирова..ый, восторже..ый смех."
+						}
+					],
+					"correct-answers": "1,4,5",
+					"explaining": "explaining"
+				},
+				{
+					"question": "Пишется не раздельно во всех случаях в рядах:",
+					"answers": [
+						{
+							"1":"(не)лёгкая, но интересная судьба; (не)настная погода",
+							"2":"(не)главные, а второстепенные приметы; (не)встретив друзей",
+							"3":"отнюдь (не)сложные задачи; рассказ автором (не)завершён",
+							"4":"ещё (не)сбитые ветром листья; (не)гранитный памятник",
+							"5":"абсолютно (не)интересный; (не)прекращающиеся пожары"
+						}
+					],
+					"correct-answers": "2,3,4",
+					"explaining": "explaining"
+				},
+				{
+					"question": "Укажите номера пропусков, где пишется не:",
+					"answers": [
+						{
+							"1":"В	прошлом году Игорь н.. раз выходил в финал,",
+							"2":"но так и н.. стал победителем",
+							"3":"Мы н.. могли ",
+							"4":"н.. переживать за него",
+							"5":"Сколько бы он н.. выступал на соревнованиях раньше, он всегда побеждал"
+						}
+					],
+					"correct-answers": "1,2,3,4",
+					"explaining": "explaining"
+				},
+				{
+					"question": "Через дефис пишутся слова:",
+					"answers": [
+						{
+							"1":"кофе (по)варшавски",
+							"2":"(южно)африканский спортсмен",
+							"3":"(полу)улыбка",
+							"4":"(ядовито)зелёный",
+							"5":"(контр)довод"
+						}
+					],
+					"correct-answers": "1,2,5",
+					"explaining": "explaining"
+				},
+				{
+					"question": "Раздельно пишутся выделенные слова в предложениях:",
+					"answers": [
+						{
+							"1":"Актёры работали в полную силу, были точны и хватали <b>(с)лёту</b> замечания режиссёра.",
+							"2":"Один читал <b>(в)слух</b>, остальные слушали.",
+							"3":"Здесь группами и <b>(в)одиночку</b> росли молодые дубки",
+							"4":"Тучи ползли по небу <b>так(же)</b> медленно и скучно, как и раньше.",
+							"5":"Надо сделать всё, <b>что(бы)</b> сдать объект до наступления зимы."
+						}
+					],
+					"correct-answers": "1,3,4",
+					"explaining": "explaining"
+				},
+				{
+					"question": "",
+					"answers": [
+						{
+							"1":"",
+							"2":"",
+							"3":"",
+							"4":"",
+							"5":""
+						}
+					],
+					"correct-answers": ""
+				}
+				
+				
+				
+			]
+		},
+		
+		{
+			"variant": "2",
+			"questions" : [ 
+				{
+					"question": "Бишется ь на месте пропуска в словах:",
+					"answers": [
+						{
+							"1":"пижамоч..ка",
+							"2":"бур..ян",
+							"3":"увлеч..cя",
+							"4":"много неудач..",
+							"5":"без..ядерный"
+						}
+					],
+					"correct-answers": "2,3"
+				},
+				{
+					"question": "Двойные согласные есть в словах:",
+					"answers": [
+						{
+							"1":"ра(с/сс)писаться в журнале",
+							"2":"бе(с/сс)мысленный",
+							"3":"га(л/лл)ерея",
+							"4":"кора(л/лл)овый",
+							"5":"эскимо(с/сс)кий"
+						}
+					],
+					"correct-answers": "2,4,5"
+				},
+				{
+					"question": "Пишется и на месте пропуска в словах:",
+					"answers": [
+						{
+							"1":"пр..клеить к конверту",
+							"2":"пр..стыдить лентяя",
+							"3":"из пр..данья старины глубокой",
+							"4":"пр..образовать что-либо",
+							"5":"пр..вивка против гриппа"
+						}
+					],
+					"correct-answers": "2,3"
+				},
+				{
+					"question": "",
+					"answers": [
+						{
+							"1":"",
+							"2":"",
+							"3":"",
+							"4":"",
+							"5":""
+						}
+					],
+					"correct-answers": ""
+				}
+				
+				
+			]
+		},
+		
+		{
+			"variant": "3",
+			"questions" : [ 
+				{
+					"question": "Вишется ь на месте пропуска в словах:",
+					"answers": [
+						{
+							"1":"пижамоч..ка",
+							"2":"бур..ян",
+							"3":"увлеч..cя",
+							"4":"много неудач..",
+							"5":"без..ядерный"
+						}
+					],
+					"correct-answers": "2,3"
+				},
+				{
+					"question": "Двойные согласные есть в словах:",
+					"answers": [
+						{
+							"1":"ра(с/сс)писаться в журнале",
+							"2":"бе(с/сс)мысленный",
+							"3":"га(л/лл)ерея",
+							"4":"кора(л/лл)овый",
+							"5":"эскимо(с/сс)кий"
+						}
+					],
+					"correct-answers": "2,4,5"
+				},
+				{
+					"question": "Пишется и на месте пропуска в словах:",
+					"answers": [
+						{
+							"1":"пр..клеить к конверту",
+							"2":"пр..стыдить лентяя",
+							"3":"из пр..данья старины глубокой",
+							"4":"пр..образовать что-либо",
+							"5":"пр..вивка против гриппа"
+						}
+					],
+					"correct-answers": "2,3"
+				},
+				
+				{
+					"question": "",
+					"answers": [
+						{
+							"1":"",
+							"2":"",
+							"3":"",
+							"4":"",
+							"5":""
+						}
+					],
+					"correct-answers": ""
+				}
+				
+				
+			]
+		}
+	]
+}
+
+
+
+var currentVariant = null;
+var currentQuestion = 0;
+
+
+function startTest () {
+	
+}
+
+$('.start-test').on('click', function() {
+		var variantsColumn = document.getElementsByClassName("variants-column")[0];
+		for ( var i = 0; i < variantsColumn.children.length; i++ ) {
+		if (variantsColumn.children[i].querySelector("input").checked) {
+			$('.stage-2').css('display', 'none');
+			$('.stage-3').css('display', 'flex');
+			currentVariant = i;
+			
+			$('.question-field').html(json.data[currentVariant].questions[currentQuestion].question);
+				for (var j = 1; j <= 5; j++ ) {
+					var answerVariantDiv = document.createElement('div');
+					answerVariantDiv.className = "answer-" + j;
+					var answerVariantInput = document.createElement('input');
+					answerVariantInput.type = "checkbox";
+					answerVariantInput.className = "answer-checkbox-" + j;
+					var answerVariantValue = document.createElement('span');
+					answerVariantValue.className = "answer-value-" + j;
+					answerVariantValue.innerHTML = json.data[currentVariant].questions[currentQuestion].answers[0][j];
+
+
+					answerVariantDiv.appendChild(answerVariantInput);
+					answerVariantDiv.appendChild(answerVariantValue);
+					$('.variants-field').append(answerVariantDiv);
+				}
+			
+			
+			
+		}
+	}
+});
+
+function rewriteQuestion () {
+		for (var j = 1; j <= 5; j++ ) {
+				$('.question-field').html(json.data[currentVariant].questions[currentQuestion].question);
+			
+				
+				var answerVariantInput = document.getElementsByClassName("answer-checkbox-" + j)[0];
+				var answerVariantValue = document.getElementsByClassName("answer-value-" + j)[0];
+				answerVariantValue.innerHTML = json.data[currentVariant].questions[currentQuestion].answers[0][j];
+				answerVariantInput.checked = false;
+
+				
+		}
+}
+var correct = 0;
+var wrong = 0;
+var errors = "";
+$('.answer-button').on('click', function() {
+	var answerString = [];
+	
+	for (var i = 1; i <= 5; i++) {
+		var answerVariantCheckbox = document.getElementsByClassName('answer-' + i)[0].querySelector('input');
+		if (answerVariantCheckbox.checked) {
+			answerString.push(i);
+			
+			
+		}
+//		console.log(answerVariantCheckbox.checked);
+	}
+	if (answerString.toString() === json.data[currentVariant].questions[currentQuestion]["correct-answers"]) {
+		
+		correct++;
+		$('.correct').html(correct);
+		currentQuestion++;
+		rewriteQuestion();
+
+		if (currentQuestion >= json.data[currentVariant].questions.length-1) {
+			$('.stage-3').css('display', 'none');
+			$('.stage-4').css('display', 'flex');
+			$('.correct-answers').html("Правильных ответов: " + correct);
+			$('.wrong-answers').html("Неправильных ответов: " + wrong);
+			if (wrong === 0) {
+				$('.diary').css('display', 'none');
+			}
+		}
+		
+	} else {
+		
+		wrong++;
+		$('.wrong').html(wrong);
+		currentQuestion++;
+		console.log(currentQuestion);
+		errors += "Вопрос " + currentQuestion + ":" + "<br>"
+		+ json.data[currentVariant].questions[currentQuestion-1].question + "<br>"
+		+ "-" + json.data[currentVariant].questions[currentQuestion-1].answers[0][1] + "<br>"
+		+ "-" + json.data[currentVariant].questions[currentQuestion-1].answers[0][2] + "<br>"
+		+ "-" + json.data[currentVariant].questions[currentQuestion-1].answers[0][3] + "<br>"
+		+ "-" + json.data[currentVariant].questions[currentQuestion-1].answers[0][4] + "<br>"
+		+ "-" + json.data[currentVariant].questions[currentQuestion-1].answers[0][5] + "<br>"
+		+ "Отвечено: " + answerString.toString() + "<br>"
+		+ "Правильный ответ: " + json.data[currentVariant].questions[currentQuestion-1]["correct-answers"] + "<br>" 
+		+ json.data[currentVariant].questions[currentQuestion-1]["explaining"] + "<br><br>";
+		$('.errors').html(errors);
+		
+		rewriteQuestion();
+		if (currentQuestion >= json.data[currentVariant].questions.length-1) {
+			$('.stage-3').css('display', 'none');
+			$('.stage-4').css('display', 'flex');
+			$('.correct-answers').html("Правильных ответов: " + correct);
+			$('.wrong-answers').html("Неправильных ответов: " + wrong);
+		}
+	
+	}
+	
+	
+});
+
+
+$('.menu-button').on('click', function() {
+	currentVariant = null;
+	currentQuestion = 0;
+	errors = "";
+	
+	correct = 0;
+	$('.correct').html(correct);
+	wrong = 0;
+	$('.wrong').html(wrong);
+	
+	$('.diary').css('display', 'flex');
+	
+	$('.question-field').html("");
+	$('.variants-field').html("");
+	
+	$('.stage-3').css('display', 'none');
+	$('.stage-4').css('display', 'none');
+	$('.stage-2').css('display', 'flex');
+});
+
+$('.diary').on('click', function() {
+	$('.stage-4').css('display', 'none');
+	$('.stage-5').css('display', 'flex');
+	
+	$('.close').on('click', function() {
+		$('.stage-5').css('display', 'none');
+		$('.stage-4').css('display', 'flex');
+	});
+});
+
+
+
+
